17b5c0e5c98efd268d50def52d101f34fbf921bf
Ilya <ktoto47913@gmail.com>
2023-08-12 23:14:47 +0300

    123

--- HomeWork 2.07/ViewControllers/QuestionsViewController.swift
+++ HomeWork 2.07/ViewControllers/QuestionsViewController.swift
@@ -8,7 +8,129 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
+    // MARK: - IB Outlets
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    // MARK: - Private Properties
+    private let questions = Question.getQuestions()
+    
+    private var questionIndex = 0
+    
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateQuestionsVC()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultViewController = segue.destination as? ResultViewController else {
+            return
+        }
+        
+        resultViewController.resultText = answersChosen
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func pressSingleAnswerButton(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        answersChosen.append(currentAnswers[buttonIndex])
+        
+        nextQuestion()
+    }
+    
+    @IBAction func pressMultipleAnswerButton() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func pressRangedAnswerButton() {
+        let index = lrintf(rangedSlider.value)
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+    
+    // MARK: - Private Methods
+    private func updateQuestionsVC() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)."
+        
+        let currentQuestion = questions[questionIndex]
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        questionLabel.text = currentQuestion.title
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateQuestionsVC()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+}
+
+// MARK: - Methods For Displaying Screen Elements
+private extension QuestionsViewController {
+    func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden.toggle()
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangedStackView(with: currentAnswers)
+        }
     }
 }
--- HomeWork 2.07/ViewControllers/ResultViewController.swift
+++ HomeWork 2.07/ViewControllers/ResultViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    @IBOutlet var resultLabel: [UILabel]!
+    
+    var resultText: [Answer] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
+        
+        for result in resultText {
+            var a: Character = " "
+            var b = ""
+            var num = 0
+            
+            if a != result.animal.rawValue {
+                a = result.animal.rawValue
+                
+            } else {
+                b = result.animal.definition
+                print(a)
+                print(b)
+            }
+            
+            
+        }
     }
+    
+    
 }
